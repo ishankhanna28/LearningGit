a28c12adf8ca958c3609ab90fe1bde0f152debba
ishankhanna28 <ishankhanna28@gmail.com>
2021-03-14 01:37:44 +0530

    updated for_loops with new algos

--- for_loops.cpp
+++ for_loops.cpp
@@ -5,6 +5,11 @@ int main()
 {
 	int n=5;
 	while(n--)
-	cout<<"Hello World"<<endl;
+		cout<<"Hello World"<<endl;
+
+	cout<<"done with for loops"<<endl;
+	cout<<"wait, What abour for loops?"
+
+
 	return 0;
 }
